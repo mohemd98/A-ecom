c31110682c59e15de57a4a13b53489eaed372cdc
mohemd98 <mohemd.web@gmail.com>
2023-12-18 16:51:13 +0300

    2023/12/17.2

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,8 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+    public const ADMIN = '/admin';
+
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
@@ -35,6 +37,12 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::prefix('admin')
+                ->middleware(['admin'])
+                ->namespace($this->namespace)
+                ->group(base_path('routes/admin.php'));
+
         });
     }
 }
